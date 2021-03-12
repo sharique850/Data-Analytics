9aff525ffae78887520ea1259682ae7010af3e37
Sharique Akhter <60446567+sharique850@users.noreply.github.com>
2021-03-12 12:44:57 +0530

    Add files via upload

---
+++ lab2.r
@@ -0,0 +1,7 @@
+#Program 1
+{
+  name = readline(prompt = "Enter Your name ")
+  age = readline(prompt = "Enter Your age ")
+  address = readline(prompt = "Enter Your address")
+  print(paste("My name is",name,"I am",age,"Year old","and my address is",address))
+}
